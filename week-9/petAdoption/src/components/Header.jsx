1520fec9f17f0ddb93829b13fc8a7157309fb1d2
anshsgit <anshsmail738@gmail.com>
2025-03-29 01:58:55 +0530

    update components

--- week-9/petAdoption/src/components/Header.jsx
+++ week-9/petAdoption/src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const Header = () => {
+const Header = (props) => {
+
+  const style = {backgroundColor: 'rgb(196, 164, 132, 0.75)', padding: '20px', marginBottom: '25px'}
   return (
-    <div>Header</div>
+    <div>
+      <h2 style={style}>{props.message}</h2>
+    </div>
   )
 }
 
