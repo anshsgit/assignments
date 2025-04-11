66299299b1227052f8051e4cba9ba68a7b5728fd
anshsgit <anshsmail738@gmail.com>
2025-04-11 12:48:37 +0530

    modified Login.jsx

--- week-10/authSystem/src/components/Login.jsx
+++ week-10/authSystem/src/components/Login.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
 
-const Login = () => {
+const Login = ({loggedin: propLoggedin}) => {
+
+  const username = React.useRef(null);
+  const password = React.useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const loginValues = {username: username.current.value,
+    password: password.current.value};
+    
+      if(loginValues.username && loginValues.password) {
+        propLoggedin(loginValues);
+      }
+      else {
+        alert('Fill both the details.');
+      }
+
+  }
+
   return (
-    <div>Login</div>
+    <div class='login'>
+
+      <h2>Login Form</h2>
+        <form class='loginForm' onSubmit={handleSubmit}>
+
+        <label htmlFor='username'>Username: </label>
+        <input id='username' type='text' placeholder='USERNAME' ref={username} /> <br />
+
+        <label htmlFor='password'>Password: </label>
+        <input id='password' type='text' placeholder='PASSWORD' ref={password} /> <br />
+
+        <input type='submit'/>
+        </form>
+    </div>
   )
 }
 
