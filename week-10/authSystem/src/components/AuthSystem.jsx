74b78a27837c2866e868ea44029f0c1967447255
anshsgit <anshsmail738@gmail.com>
2025-04-11 12:48:17 +0530

    modified Home.jsx

--- week-10/authSystem/src/components/AuthSystem.jsx
+++ week-10/authSystem/src/components/AuthSystem.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, {useState, createContext, useContext} from 'react';
+import AppBar from './AppBar';
+import Home from './Home';
+import Login from './Login';
+
+export const AuthContext = createContext(null);
+
+function ContextProvider({contextValue, children}) {
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
 
 const AuthSystem = () => {
+  const [login, setLogin] = useState({username: '', password: ''});
+  const [isLogin, setIsLogin] = useState(false);
+  const [useContextApi, setUseContextApi] = useState(false);
+
+  function loggedin({username, password}) {
+    setLogin(login => ({username, password}));
+    setIsLogin(true);
+  }
+
+  function loggedout() {
+    setLogin(login => ({username: '', password: ''}));
+    setIsLogin(false);
+  }
+
+  const contextValue = useContextApi? {login, isLogin, loggedin, loggedout} : null;
+
   return (
-    <div>AuthSystem</div>
-  )
+    <div>
+      <ContextProvider contextValue = {contextValue}>
+        <AppBar login={login} isLogin={isLogin} useContextApi={useContextApi} setUseContextApi={setUseContextApi} loggedout={loggedout} />
+        {isLogin? (<Home />) : (<Login loggedin={loggedin} useContextApi={useContextApi} />)}
+      </ContextProvider>
+    </div>
+  );
 }
 
 export default AuthSystem
--- week-10/authSystem/src/components/Home.jsx
+++ week-10/authSystem/src/components/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import AppBar from './AppBar.jsx'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <main>
+        <h2>Welcome to the Auth System Demo</h2>
+        <p>The demo showcases two approaches to managing authentication state in react:</p>
+        <ul>
+          <li>State Lifting</li>
+          <li>Context API</li>
+        </ul>
+        <p>Use the toggle above to switch between the two approaches.</p>
+    </main>
   )
 }
 
